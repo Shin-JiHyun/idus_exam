27da4231df2cd6dadfcbaba2a9b9a1c646edefe5
= <sjhyun0926@gmail.com>
2025-02-12 11:45:29 +0900

    [feat] swagger 설정

--- src/main/java/com/example/idus_exam/config/SwaggerConfig.java
+++ src/main/java/com/example/idus_exam/config/SwaggerConfig.java
@@ -1,55 +1,93 @@
 package com.example.idus_exam.config;
 
+import com.example.idus_exam.config.filter.LoginFilter;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-//import io.swagger.v3.oas.models.security.SecurityRequirement;
-//import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-    @Configuration
-    public class SwaggerConfig {
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Optional;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenApiCustomizer springSecurityLoginEndpointCustomizer(ApplicationContext applicationContext) {
+        FilterChainProxy springSecurityFilterChain = applicationContext.getBean("springSecurityFilterChain", FilterChainProxy.class);
+
+        return (openApi) -> {
+            for (SecurityFilterChain filterChain : springSecurityFilterChain.getFilterChains()) {
+                // 스프링 시큐리티의 특정 필터를 받아오는 부분
+                Optional<LoginFilter> filter = filterChain.getFilters().stream()
+                        .filter(LoginFilter.class::isInstance)
+                        .map(LoginFilter.class::cast)
+                        .findAny();
+                if(filter.isPresent()) {
+                    // 문서 설정 객체
+                    Operation operation = new Operation();
+
+                    // 문서에서 요청 설정
+                    Schema<?> schema = new ObjectSchema()
+                            .addProperty("email", new StringSchema().example("example2@example.com"))
+                            .addProperty("password", new StringSchema().example("Password123!"));
+                    RequestBody requestBody = new RequestBody().content(
+                            new Content().addMediaType("application/json", new MediaType().schema(schema)));
+
+                    operation.setRequestBody(requestBody);
 
 
-        // url: http://localhost:8080/swagger-ui/index.html#/
-        @Bean
-        public OpenAPI getOpenApi() {
-            Server server = new Server().url("/");
-            return new OpenAPI()
-                    .info(getSwaggerInfo())
-                    .components(getComponents());
-//                // 보안 인증 추가 시 사용
-//                .components(authSetting())
-//                .addServersItem(server)
-//                .addSecurityItem(new SecurityRequirement().addList("access-token"));
-        }
+                    // 문서에서 응답 설정
+                    ApiResponses response = new ApiResponses();
+                    response.addApiResponse(
+                            String.valueOf(HttpStatus.OK.value()),
+                            new ApiResponse().description(HttpStatus.OK.getReasonPhrase())
+                    );
+                    response.addApiResponse(
+                            String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                            new ApiResponse().description(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    );
+                    operation.setResponses(response);
 
-        private Info getSwaggerInfo() {
-            License license = new License();
-            license.setName("{Application}");
 
-            return new Info()
-                    .title("IDUS API Document")
-                    .description("Pill-Mate Server's API document.")
-                    .version("v0.0.1")
-                    .license(license);
-        }
+                    // 직접 만든 필터의 문서를 swagger에 등록
+                    operation.addTagsItem("회원 기능");
+                    operation.summary("로그인 기능");
+                    PathItem pathItem = new PathItem().post(operation);
+                    openApi.getPaths().addPathItem("/login", pathItem);
 
-        private Components getComponents() {
-            return new Components();
-        }
-//    // 보안 인증 추가 시 사용
-//    private Components authSetting() {
-//        return new Components()
-//                .addSecuritySchemes(
-//                        "access-token",
-//                        new SecurityScheme()
-//                                .type(SecurityScheme.Type.HTTP)
-//                                .scheme("bearer")
-//                                .bearerFormat("JWT"));
-//    }
+                }
+
+            }
+        };
     }
+
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("A 프로젝트 기능 테스트")
+                .description("A 프로젝트 기능을 테스트 하기 위한 웹 페이지")
+
+                .version("1.0.0");
+    }
+}
 
